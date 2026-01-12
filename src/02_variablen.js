f2343b5d49ba9577a5e323778ae034de4a883d5e
Jennifer <jenniferroesner85@yahoo.com>
2026-01-12 09:43:23 +0000

    Variable vs. konstanten

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -16,29 +16,40 @@
 /* Deklaration + Wertzuweisung I */
 
 
-/*let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | assignment
-console.log(firstName);  // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!");  // Ausgabe
-
+// /*let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung | assignment
+// console.log(firstName);  // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!");  // Ausgabe
+ 
 /*  
     Wertzuweisung II + Datentypen 
     JS ist ein untypisierte Sprache!
 */
 
-let test; //Deklaration
+// let test; //Deklaration
 
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
-
-
-
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
 
 
 
 // Variablen vs. Konstanten in JS 
+
+
+ // Variable
+// let test;  // Variable : Deklaration
+// test = "hi"; // Wertzuweisung (Initialisierung)
+// test = "hello"; // Überschreiben
+// console.log("Inhalt: " + test); // Ausgabe
+
+// // Konstante
+// const test2 = "Hi!"; // Konstante : Deklaration + Wertzuweisung 
+// //test2 = "hello!"; // Fehler! Überschreiben nicht möglich!
+// console.log("Inhalt: " +test2); // Ausgabe
+
+
 
