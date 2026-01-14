12649fe764f4cc4d9d3e677126d536e9df663a10
Jennifer <jenniferroesner85@yahoo.com>
2026-01-14 09:29:45 +0000

    work_request

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -18,3 +18,84 @@ isJohnEqual = (ageJohn == ageMark);
 // console.log("isJohnEqual: " + isJohnEqual);
 // console.log("------------------");
 
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter.");
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if (true) 
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//    console.log("John ist älter.");  
+// } 
+// else 
+// {
+//    console.log("John ist jünger.");  
+// }
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if(isJohnOlder)
+// {
+//     console.log("John ist älter.");  
+// } 
+// else if(isJohnEqual)  // alternative Zweig
+// {
+//     console.log("John ist gleich alt.");  
+// }
+// else 
+// {
+//    console.log("John ist jünger.");  
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+// const firstName = "Jane";
+// let job;
+
+// job = "driver";      // .. fährt TAXI! / UBER
+// job = "diver";       // .. taucht im Rhein!
+// job = "artist";      // .. malt ein Bild!
+// job = "pilot";       // .. macht etwas anderes! --> default
+// job = "teacher";     // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+
+switch (job) 
+{
+    case "driver":   // job === "driver"
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver":   // job === "diver"
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":   // job === "artist"
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "pilot":   // job === "pilot"
+        console.log(firstName + " fliegt ein Flugzeug!");
+        break;
+    case "teacher":   // job === "teacher"
+        console.log(firstName + " unterrichtet!");
+        break;
+    case "instructor":   // job === "instructor"
+        console.log(firstName + " unterrichtet!");
+        break;
+    default:
+        console.log(firstName + " macht etwas anderes!");
+}
